c34b5ecbb18d4a611da29a90b7cdf30b2b6280e3
leila-wilde <leila.wilde@laplateforme.io>
2025-11-20 15:24:05 +0000

    fix: changed sql initilisation file names so that they execute in the correct order in MySql

--- db/01_init_struct.sql
+++ db/01_init_struct.sql
@@ -11,27 +11,27 @@ USE inframusic_db;
 /* Create tables for the inframusic_db database */
 
 CREATE TABLE genres (
-    genre_id INT NOT NULL,
+    genre_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     CONSTRAINT PK_genre PRIMARY KEY (genre_id)
 );
 
 CREATE TABLE artists (
-    artist_id INT NOT NULL,
+    artist_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     CONSTRAINT PK_artist PRIMARY KEY (artist_id)
 );
 
 CREATE TABLE albums (
-    album_id INT NOT NULL,
+    album_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     artist_id INT NOT NULL,
-    CONSTRAINT PK_album PRIMARY KEY (album_id),
+    -- CONSTRAINT PK_album PRIMARY KEY (album_id),
     CONSTRAINT FK_artist_id FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
 
 CREATE TABLE tracks (
-    track_id INT NOT NULL,
+    track_id INT NOT NULL AUTO_INCREMENT,
     album_id INT,
     genre_id INT,
     name VARCHAR(255) NOT NULL,
